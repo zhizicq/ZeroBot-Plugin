d6210144a5d83e364fa7e5969925082c63cdf739
zhizicq <a3692602@gmail.com>
2022-05-26 16:49:31 +0800

    Update setu_geter.go
    Add imgur api

--- plugin/setutime/setu_geter.go
+++ plugin/setutime/setu_geter.go
@@ -7,7 +7,12 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"flag"
+	"fmt"
+	"net/http"
 
+	"github.com/koffeinsource/go-imgur"
+	"github.com/koffeinsource/go-klogger"
 	"github.com/FloatTech/AnimeAPI/pixiv"
 	sql "github.com/FloatTech/sqlite"
 	control "github.com/FloatTech/zbputils/control"
@@ -47,7 +52,36 @@ var pool = &imgpool{
 	max:  10,
 	pool: make(map[string][]*message.MessageSegment),
 }
+func upload(){
+	imgurClientID := flag.String("id", "", "Your imgur client id. REQUIRED!")
+	url := flag.String("url", "", "Gets information based on the URL passed.")
+	upload := flag.String("upload", "", "Filepath to an image that will be uploaded to imgur.")
+	image := flag.String("image", "", "The image ID to be queried.")
+	album := flag.String("album", "", "The album ID to be queried.")
+	gimage := flag.String("gimage", "", "The gallery image ID to be queried.")
+	galbum := flag.String("galbum", "", "The gallery album ID to be queried.")
+	rate := flag.Bool("rate", false, "Get the current rate limit.")
+	flag.Parse()
 
+	// Check if there is anything todo
+	if flag.NFlag() >= 3 || *imgurClientID == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	client := new(imgur.Client)
+	client.HTTPClient = new(http.Client)
+	client.Log = new(klogger.CLILogger)
+	client.ImgurClientID = *imgurClientID
+
+	if *upload != "" {
+		_, st, err := client.UploadImageFromFile(*upload, "", "test title", "test desc")
+		if st != 200 || err != nil {
+			fmt.Printf("Status: %v\n", st)
+			fmt.Printf("Err: %v\n", err)
+		}
+	}	
+}
 func init() { // 插件主体
 	engine := control.Register("setutime", &control.Options{
 		DisableOnDefault: false,
